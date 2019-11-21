b67bd8a5f35b8e79d3952b2add84f5c0a2fbf5b4
Ron <vronlui@gmail.com>
2019-11-21 13:55:19 +1100

    Update code style
    Cleaned up code style of .R files using Rstudio 'style' addin.

--- app.R
+++ app.R
@@ -1,10 +1,12 @@
 library("pacman")
-pacman::p_load("shiny", "shinydashboard", "shinycssloaders", "ggplot2", 
-               "MetaboAnalystR", "KEGGREST", "tidyverse", "ggfortify",
-               "httr", "XML", "DT")
+pacman::p_load(
+  "shiny", "shinydashboard", "shinycssloaders", "ggplot2",
+  "MetaboAnalystR", "KEGGREST", "tidyverse", "ggfortify",
+  "httr", "XML", "DT"
+)
 
 # set working directory
-#setwd('./pathway-shinyapp/')
+# setwd('./pathway-shinyapp/')
 
 # source functions, ui.R and server.R files
 source("helper-func.R")
